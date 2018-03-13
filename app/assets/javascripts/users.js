aa236b7bf4e68e01ac794beb0c63d5d98755cf12
Dominic Soesatyo <dom.soesatyo@gmail.com>
2018-03-13 15:55:37 +0000

    Added server side code for creating subscriptions for pro users

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -6,7 +6,6 @@ $(document).on('turbolinks:load', function(){
   
   //Set Stripe public key.
   Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
-  
   //When user clicks form submit btn,
   submitBtn.click(function(event){
     //prevent default submission behavior.
@@ -45,29 +44,26 @@ $(document).on('turbolinks:load', function(){
       submitBtn.prop('disabled', false).val("Sign Up");
     } else {
       //Send the card info to Stripe.
-    Stripe.createToken({
-      number: ccNum,
-      cvc: cvcNum,
-      exp_month: expMonth,
-      exp_year: expYear
-    }, stripeResponseHandler);
-      
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
     }
-  
-    
     return false;
   });
   
   //Stripe will return a card token.
-  function stripeResponseHandler(status, response){
-    //Get the token from the response
+  function stripeResponseHandler(status, response) {
+    
+    //Get the token from the response.
     var token = response.id;
     
+    //Inject the card token in a hidden field.
+    theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
     
-  //Inject card token into hidden field
-  theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
-
-  //Submit form to our Rails app.
-  theForm.get(0).submit();
-}
+    //Submit form to our Rails app.
+    theForm.get(0).submit();
+  }
 });
